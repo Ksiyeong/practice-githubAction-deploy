bb500651e38e345003f6dab3bd76561dc572d952
Ksiyeong <yuiop2767@naver.com>
2022-12-08 15:02:18 +0900

    HelloController 색상변경

--- src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
+++ src/main/java/com/codestates/practicegithubActiondeploy/controller/HelloController.java
@@ -10,7 +10,13 @@ public class HelloController {
 public String hello() {
         return "<html>\n" +
                 "<body>\n" +
+                "<h1 style=\"color: red\">Hello Siyeong World !!</h1>\n" +
+                "<h1 style=\"color: orange\">Hello Siyeong World !!</h1>\n" +
+                "<h1 style=\"color: yellow\">Hello Siyeong World !!</h1>\n" +
                 "<h1 style=\"color: green\">Hello Siyeong World !!</h1>\n" +
+                "<h1 style=\"color: blue\">Hello Siyeong World !!</h1>\n" +
+                "<h1 style=\"color: navy\">Hello Siyeong World !!</h1>\n" +
+                "<h1 style=\"color: purple\">Hello Siyeong World !!</h1>\n" +
                 "</body>\n" +
                 "</html>";
     }
